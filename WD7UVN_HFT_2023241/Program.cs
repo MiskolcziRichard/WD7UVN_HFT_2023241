e61d6e305eddefdb2aeeee846ccef56258496cbd
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2023-10-03 10:36:32 +0200

    Adding files

---
+++ WD7UVN_HFT_2023241/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WD7UVN_HFT_2023241
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
