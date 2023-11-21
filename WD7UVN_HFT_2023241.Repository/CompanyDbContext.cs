01fc34ad64e051d9edb5108727b61e3297b1c158
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2023-11-21 12:25:31 +0100

    Creating DbContext

---
+++ WD7UVN_HFT_2023241.Repository/CompanyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using WD7UVN_HFT_2023241.Models;
+
+namespace WD7UVN_HFT_2023241.Repository
+{
+    public class CompanyDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<MaintainerTeam> Maintainers { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
