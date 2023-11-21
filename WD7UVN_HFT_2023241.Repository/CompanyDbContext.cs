ab8f6f7447d3c3d28ed9dd99a73ef5918223811e
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2023-11-21 12:33:46 +0100

    Create InMemoryDb connection

--- WD7UVN_HFT_2023241.Repository/CompanyDbContext.cs
+++ WD7UVN_HFT_2023241.Repository/CompanyDbContext.cs
@@ -7,9 +7,29 @@ namespace WD7UVN_HFT_2023241.Repository
 {
     public class CompanyDbContext : DbContext
     {
+        //Tables
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<MaintainerTeam> Maintainers { get; set; }
         public DbSet<Service> Services { get; set; }
+
+        public CompanyDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                        .UseInMemoryDatabase("company");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
