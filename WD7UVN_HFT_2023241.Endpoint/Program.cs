58b21073dbad417e63ed9f4e2531e465671b9e63
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2023-11-22 11:42:09 +0100

    Removing unneccesary using directives

--- WD7UVN_HFT_2023241.Endpoint/Program.cs
+++ WD7UVN_HFT_2023241.Endpoint/Program.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using WD7UVN_HFT_2023241.Repository;
 
 namespace WD7UVN_HFT_2023241.Endpoint
