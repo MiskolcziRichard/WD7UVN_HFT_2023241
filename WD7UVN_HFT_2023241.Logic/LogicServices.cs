1a1f7ff0a4c5f7157e13f7239313e779f8be93c4
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2023-11-22 14:01:22 +0100

    Creating LoginServices with queries inside

---
+++ WD7UVN_HFT_2023241.Logic/LogicServices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using WD7UVN_HFT_2023241.Models;
+using WD7UVN_HFT_2023241.Repository;
+
+namespace WD7UVN_HFT_2023241.Logic
+{
+    public interface ILogicServices
+    {
+        public ICRUD CRUDOperations { get; set; }
+
+        //additional, non-CRUD operations
+
+        public IQueryable<Employee> WhoWorksInMaintainerTeam(int maintainerTeamId)
+        {
+            return CRUDOperations
+            .ReadAllEmployees()
+            .Where(e => e.MAINTAINER_ID == maintainerTeamId);
+        }
+
+        public IQueryable<Employee> GetSubordinates(int managerId)
+        {
+            return CRUDOperations
+            .ReadAllEmployees()
+            .Where(e => e.MANAGER_ID == managerId);
+        }
+
+        public IQueryable<Customer> WhoUsesService(int serviceId)
+        {
+            return CRUDOperations
+            .ReadAllCustomers()
+            .Where(c => c.SERVICE_ID == serviceId);
+        }
+    }
+
+    public class LogicServices : ILogicServices
+    {
+        public ICRUD CRUDOperations { get; set; }
+
+        public LogicServices(ICRUD CRUDOperations)
+        {
+            this.CRUDOperations = CRUDOperations;
+        }
+    }
+}
