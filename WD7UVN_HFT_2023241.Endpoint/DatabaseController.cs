cb4ced73d91fde58c55f1d3ce7f33b613b4c4cd9
TypoMustakes <miskolczi.richard@proton.me>
2023-12-05 14:25:27 +0100

    Removing all non-employee related routes

--- WD7UVN_HFT_2023241.Endpoint/DatabaseController.cs
+++ WD7UVN_HFT_2023241.Endpoint/DatabaseController.cs
@@ -7,20 +7,14 @@ namespace WD7UVN_HFT_2023241.Endpoint
 {
     [ApiController]
     [Route("api/database")]
-    public class DatabaseController : ControllerBase
+    public class EmployeeController : ControllerBase
     {
         public ILogicServices LogicServices { get; set; }
 
-        public DatabaseController(ILogicServices LogicServices)
+        public EmployeeController(ILogicServices LogicServices)
         {
             this.LogicServices = LogicServices;
         }
-
-		[HttpGet("WhoWorksInMaintainerTeam/{id}")]
-		public IQueryable<Employee> WhoWorksInMaintainerTeam(int id)
-		{
-			return LogicServices.WhoWorksInMaintainerTeam(id);
-		}
 
 		[HttpGet("GetSubordinates/{id}")]
 		public IQueryable<Employee> GetSubordinates(int id)
@@ -28,128 +22,32 @@ namespace WD7UVN_HFT_2023241.Endpoint
 			return LogicServices.GetSubordinates(id);
 		}
 
-		[HttpGet("WhoUsesService")]
-		public IQueryable<Customer> WhoUsesService(int id)
-		{
-			return LogicServices.WhoUsesService(id);
-		}
-
-        [HttpGet("Employees")]
-        public IQueryable<Employee> ReadAllEmployees()
+        [HttpGet()]
+        public IActionResult ReadAllEmployees()
         {
-            return LogicServices.CRUDOperations.ReadAllEmployees();
+            return View(LogicServices.CRUDOperations.ReadAllEmployees());
         }
 
-        [HttpGet("Services")]
-        public IQueryable<Service> ReadAllServices()
-        {
-            return LogicServices.CRUDOperations.ReadAllServices();
-        }
-
-        [HttpGet("MaintainerTeams")]
-        public IQueryable<MaintainerTeam> ReadAllMaintainerTeams()
-        {
-            return LogicServices.CRUDOperations.ReadAllMaintainerTeams();
-        }
-
-        [HttpGet("Customer")]
-        public IQueryable<Customer> ReadAllCustomers()
-        {
-            return LogicServices.CRUDOperations.ReadAllCustomers();
-        }
-
-        [HttpGet("MaintainerTeam/{id}")]
-        public MaintainerTeam ReadMaintainerTeam(int id)
-        {
-            return LogicServices.CRUDOperations.ReadMaintainerTeam(id);
-        }
-
-        [HttpGet("Service/{id}")]
-        public Service ReadService(int id)
-        {
-            return LogicServices.CRUDOperations.ReadService(id);
-        }
-
-        [HttpGet("Customer/{id}")]
-        public Customer ReadCustomer(int id)
-        {
-            return LogicServices.CRUDOperations.ReadCustomer(id);
-        }
-
-        [HttpGet("Employee/{id}")]
+        [HttpGet("{id}")]
         public Employee ReadEmployee(int id)
         {
             return LogicServices.CRUDOperations.ReadEmployee(id);
         }
 
-        [HttpPut("Employee")]
+[HttpPut()]
         public void PutEmployee([FromBody] Employee e)
         {
             LogicServices.CRUDOperations.CreateEmployee(e);
         }
 
-        [HttpPut("Customer")]
-        public void PutCustomer([FromBody] Customer c)
-        {
-            LogicServices.CRUDOperations.CreateCustomer(c);
-        }
-
-        [HttpPut("Service")]
-        public void PutService([FromBody] Service s)
-        {
-            LogicServices.CRUDOperations.CreateService(s);
-        }
-
-        [HttpPut("MaintainerTeam")]
-        public void PutMaintainerTeam([FromBody] MaintainerTeam m)
-        {
-            LogicServices.CRUDOperations.CreateMaintainerTeam(m);
-        }
-
-        [HttpPost("Employee")]
+[HttpPost()]
         public void UpdateEmployee([FromBody] Employee e)
         {
             LogicServices.CRUDOperations.UpdateEmployee(e);
         }
 
-        [HttpPost("Customer")]
-        public void UpdateCustomer([FromBody] Customer c)
-        {
-            LogicServices.CRUDOperations.UpdateCustomer(c);
-        }
-
-        [HttpPost("Service")]
-        public void UpdateService([FromBody] Service s)
-        {
-            LogicServices.CRUDOperations.UpdateService(s);
-        }
-
-        [HttpPost("MaintainerTeam")]
-        public void UpdateMaintainerTeam([FromBody] MaintainerTeam m)
-        {
-            LogicServices.CRUDOperations.UpdateMaintainerTeam(m);
-        }
-
-        [HttpDelete("Employee")]
+        [HttpDelete()]
         public void DeleteEmployee([FromBody] int id)
-        {
-            LogicServices.CRUDOperations.DeleteEmployee(id);
-        }
-
-        [HttpDelete("MaintainerTeam")]
-        public void DeleteMaintainerTeam([FromBody] int id)
-        {
-            LogicServices.CRUDOperations.DeleteMaintainerTeam(id);
-        }
-
-        [HttpDelete("Service")]
-        public void DeleteService([FromBody] int id)
-        {
-            LogicServices.CRUDOperations.DeleteEmployee(id);
-        }
-
-        [HttpDelete("Customer")]
-        public void DeleteCustomer([FromBody] int id)
         {
             LogicServices.CRUDOperations.DeleteEmployee(id);
         }
