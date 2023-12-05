3991930a6a171211951255b8b9c5908add08a870
TypoMustakes <miskolczi.richard@proton.me>
2023-12-05 11:40:01 +0100

    Configured IoC containers

--- WD7UVN_HFT_2023241.Endpoint/Startup.cs
+++ WD7UVN_HFT_2023241.Endpoint/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WD7UVN_HFT_2023241.Logic;
+using WD7UVN_HFT_2023241.Repository;
 
 namespace WD7UVN_HFT_2023241.Endpoint
 {
@@ -17,7 +14,9 @@ namespace WD7UVN_HFT_2023241.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<ILogicServices, LogicServices>();
+            services.AddTransient<ILogicServices, LogicServices>();
+            services.AddTransient<CompanyDbContext>();
+			services.AddTransient<ICRUD>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
