4513ec27c9f570650b239ae6c29d5d3767bc6cc4
TypoMustakes <miskolczi.richard@proton.me>
2023-12-06 15:02:42 +0100

    Removing unneccesary using directives

--- WD7UVN_HFT_2023241.Endpoint/Startup.cs
+++ WD7UVN_HFT_2023241.Endpoint/Startup.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using WD7UVN_HFT_2023241.Models;
 using WD7UVN_HFT_2023241.Logic;
 using WD7UVN_HFT_2023241.Repository;
 
