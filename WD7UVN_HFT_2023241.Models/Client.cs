b71634d1252a3d49dd41395562dc9b768c429f20
TypoMustakes <miskolczi.richard@proton.me>
2023-12-10 17:16:07 +0100

    Adding data attributes

--- WD7UVN_HFT_2023241.Models/Client.cs
+++ WD7UVN_HFT_2023241.Models/Client.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WD7UVN_HFT_2023241.Models
 {
     public class Client
     {
+		[Key]
         public int ID { get; set; }
+		[Required]
+        [StringLength(240)]
         public string NAME { get; set; }
+        [StringLength(240)]
         public string EMAIL { get; set; }
+        [StringLength(240)]
         public string PHONE { get; set; }
+		[ForeignKey(nameof(Service))]
         public int SERVICE_ID { get; set; }
     }
 }
