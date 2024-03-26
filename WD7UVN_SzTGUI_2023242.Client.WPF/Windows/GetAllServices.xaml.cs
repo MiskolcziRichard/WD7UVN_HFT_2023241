b175b0e24b90559882c9184cd946d3d8341590b6
MiskolcziRichard <miskolczi.richard@proton.me>
2024-03-26 13:00:50 +0100

    things

--- WD7UVN_SzTGUI_2023242.Client.WPF/Windows/GetAllServices.xaml.cs
+++ WD7UVN_SzTGUI_2023242.Client.WPF/Windows/GetAllServices.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using WD7UVN_HFT_2023241.Models;
 
 namespace WD7UVN_SzTGUI_2023242.Client.WPF
 {
@@ -19,9 +8,13 @@ namespace WD7UVN_SzTGUI_2023242.Client.WPF
     /// </summary>
     public partial class GetAllServices : Window
     {
+        public RestCollection<Service> Services { get; }
+
         public GetAllServices()
         {
             InitializeComponent();
+
+            Services = new RestCollection<Service>("https://localhost:5001", "Service", "hub");
         }
     }
 }
