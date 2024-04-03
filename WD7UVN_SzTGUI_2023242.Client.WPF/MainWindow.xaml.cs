a32958eb1f6bb6981e288c71bd0fb41e3cbed273
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2024-04-03 12:24:12 +0200

    opening up second window

--- WD7UVN_SzTGUI_2023242.Client.WPF/MainWindow.xaml.cs
+++ WD7UVN_SzTGUI_2023242.Client.WPF/MainWindow.xaml.cs
@@ -14,7 +14,8 @@ namespace WD7UVN_SzTGUI_2023242.Client.WPF
 
         private void ExpandAllServices(object sender, RoutedEventArgs e)
         {
-
+            Window window = new GetAllServices();
+            window.Show();
         }
 
         private void ExpandAllCustomers(object sender, RoutedEventArgs e)
