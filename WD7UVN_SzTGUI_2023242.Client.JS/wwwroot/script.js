4b20644236ccae8e6f2a89a1e5f1ecbb2ae0c54f
TypoMustakes <miskolczi.richard@proton.me>
2024-04-22 14:50:21 +0200

    GET and POST done

--- WD7UVN_SzTGUI_2023242.Client.JS/wwwroot/script.js
+++ WD7UVN_SzTGUI_2023242.Client.JS/wwwroot/script.js
@@ -1,3 +1,45 @@
-﻿fetch('https://localhost:5001/api/Customer')
+﻿let customers = [];
+getCustomers();
+
+async function getCustomers()
+{
+    await fetch('https://localhost:5001/api/Customer')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        customers = y;
+        console.log(customers);
+        display();
+    });
+}
+
+function display()
+{
+    document.getElementById('resultarea').innerHTML = '';
+
+    customers.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+        '<tr><td>'
+        + t.id +
+        '</td><td>'
+        + t.name +
+        '</td></tr>';
+    });
+}
+
+function addCustomer()
+{
+    let customerName = document.getElementById('customername').value;
+
+    fetch('https://localhost:5001/api/Customer', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: customerName })
+    })
+    .then(response => response)
+    .then(data =>
+    {
+        console.log("Success: ", data)
+        getCustomers();
+    })
+    .catch(error => console.error("Error: ", error));
+}
