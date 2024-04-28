c91356cdfabb4ca9112f64ca0fb149cfba718bcb
MiskolcziRichard <miskolczi.richard@stud.uni-obuda.hu>
2024-04-28 18:00:25 +0200

    A bit of organizing

--- WD7UVN_SzTGUI_2023242.Client.WPF/Windows/GetAll/GetAllServices.xaml.cs
+++ WD7UVN_SzTGUI_2023242.Client.WPF/Windows/GetAll/GetAllServices.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using WD7UVN_HFT_2023241.Models;
+using WD7UVN_SzTGUI_2023242.Client.WPF.Windows;
 
 namespace WD7UVN_SzTGUI_2023242.Client.WPF
 {
@@ -15,6 +16,12 @@ namespace WD7UVN_SzTGUI_2023242.Client.WPF
             InitializeComponent();
 
             Services = new RestCollection<Service>("http://localhost:5000/", "api/Service");
+        }
+
+        private void CreateNewService(object sender, RoutedEventArgs e)
+        {
+            Window window = new CreateNewService();
+            window.Show();
         }
     }
 }
