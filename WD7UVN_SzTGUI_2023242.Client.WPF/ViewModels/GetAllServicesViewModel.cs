93983993eb3433f8eadbde848efbde5a7b8e59b2
MiskolcziRichard <miskolczi.richard@stud.uni-obuda.hu>
2024-04-28 18:00:49 +0200

    GetAllServices view model

---
+++ WD7UVN_SzTGUI_2023242.Client.WPF/ViewModels/GetAllServicesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using WD7UVN_HFT_2023241.Models;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WD7UVN_SzTGUI_2023242.Client.WPF.ViewModels
+{
+    public class GetAllServicesViewModel
+    {
+        public RestCollection<Service> Services { get; set; }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public GetAllServicesViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Services = new RestCollection<Service>("http://localhost:5000/", "api/Service", "hub");
+            }
+        }
+    }
+}
