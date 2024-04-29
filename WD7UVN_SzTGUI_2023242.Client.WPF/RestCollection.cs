e40638e3a4280d4fc0a0dc7b0053185e654fdb6c
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2024-04-29 23:27:35 +0200

    Adding noncrud method

--- WD7UVN_SzTGUI_2023242.Client.WPF/RestCollection.cs
+++ WD7UVN_SzTGUI_2023242.Client.WPF/RestCollection.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
+using WD7UVN_HFT_2023241.Models;
 
 namespace WD7UVN_SzTGUI_2023242.Client.WPF
 {
@@ -418,6 +419,11 @@ namespace WD7UVN_SzTGUI_2023242.Client.WPF
 
         }
 
-
+        public async Task<List<Employee>> GetSubordinates(Employee e)
+        {
+            List<Employee> res = new List<Employee>();
+            res = await this.rest.GetAsync<Employee>("api/GetSubordinates");
+            return res;
+        }
     }
 }
