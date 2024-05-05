21aad617a2e7859f1f56cd8aca064bd977747fbf
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2024-05-05 18:25:18 +0200

    added create method in mainwindow code-behind

--- WD7UVN_SzTGUI_2023242.Client.WPF/MainWindow.xaml.cs
+++ WD7UVN_SzTGUI_2023242.Client.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using WD7UVN_SzTGUI_2023242.Client.WPF.ViewModels;
 using WD7UVN_SzTGUI_2023242.Client.WPF.Windows;
 
 namespace WD7UVN_SzTGUI_2023242.Client.WPF
@@ -34,6 +35,24 @@ namespace WD7UVN_SzTGUI_2023242.Client.WPF
         private void ExpandAllMaintainerTeams(object sender, RoutedEventArgs e)
         {
             Window window = new GetAllMaintainerTeams();
+            window.Show();
+        }
+
+        private void CreateNewCustomer(object sender, RoutedEventArgs e)
+        {
+            CreateNewCustomerViewModel viewModel = new CreateNewCustomerViewModel();
+            viewModel.NewCustomerCreated += (newCustomer) =>
+            {
+                var getAllCustomersViewModel = (GetAllCustomersViewModel)DataContext;
+                if (getAllCustomersViewModel != null)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        getAllCustomersViewModel.Customers.Add(newCustomer);
+                    }); 
+                }
+            };
+            Window window = new CreateNewCustomer(viewModel);
             window.Show();
         }
     }
